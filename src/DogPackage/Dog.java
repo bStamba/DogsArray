c409462146509a165206b7f979f563510c89d7e3
Brandon Stamba <brandonstamba@gmail.com>
2023-07-24 15:09:10 -0500

    Version 1 Dog Arrays

---
+++ src/DogPackage/Dog.java
@@ -0,0 +1,52 @@
+package DogPackage;
+
+public class Dog {
+
+	int dogSize;
+	String dogName;
+	String breedType;
+	String barkType;
+	String[] dog = new String[4];
+	
+	public void setName(String name) {
+		dogName = name;
+	}
+	
+	public String getName() {
+		return dogName;
+	}
+	
+	public void setSize(int size) {
+		dogSize = size;
+	}
+	
+	public int getSize() {
+		return dogSize;
+	}
+	
+	public void setBreedType(String breed) {
+		breedType = breed;		
+	}
+	
+	public String getBreedType() {
+		return breedType;
+	}
+	
+	public void setBarkType(String bark) {
+		barkType = bark;
+	}
+	
+	public String getBarkType() {
+		return barkType;
+	}
+	
+	public void setDogArray() {
+		dog[0] = Integer.toString(dogSize);
+		dog[1] = dogName;
+		dog[2] = breedType;
+		dog[3] = barkType;
+	}
+	public String[] getDogArray() {
+		return dog;
+	}
+}
---
+++ src/DogPackage/DogMain.java
@@ -0,0 +1,55 @@
+package DogPackage;
+
+public class DogMain {
+
+	public static void main(String[] args) {
+		String[] dogArray = new String[4];
+		Dog dog1 = new Dog();
+		Dog dog2 = new Dog();
+		Dog dog3 = new Dog();
+		
+		System.out.println("Welcome to Doggy Daycare, let's meet our graduating dogs!");
+		System.out.println("");
+		//Our first dog, lets set its values, use our Dog class to put them in an array, and display the results printing to console.
+		dog1.setName("Annie");
+		dog1.setSize(8);
+		dog1.setBreedType("Doberman Pincher");
+		dog1.setBarkType("Woof Woof");
+		dog1.setDogArray();
+		dogArray = dog1.getDogArray();
+		System.out.print("Our first dog is: ");
+		for(int i = 0; i < dogArray.length; i++) {
+			System.out.print(dogArray[i] + ", ");
+		}
+		System.out.println("");
+		
+		//Our second dog, lets set its values, use our Dog class to put them in an array, and display the results printing to console.
+		dog2.setName("Rose");
+		dog2.setSize(5);
+		dog2.setBreedType("Mutt");
+		dog2.setBarkType("Ruff Ruff");
+		dog2.setDogArray();
+		dogArray = dog2.getDogArray();
+		System.out.print("Our second dog is: ");
+		for(int i = 0; i < dogArray.length; i++) {
+			System.out.print(dogArray[i]);
+		}
+		System.out.println("");
+		
+		//Our third dog, lets set its values, use our Dog class to put them in an array, and display the results printing to console.
+		dog3.setName("Flo");
+		dog3.setSize(9);
+		dog3.setBreedType("Alaskan Husky");
+		dog3.setBarkType("Rrrrrrrrrrrrrr");
+		dog3.setDogArray();
+		dogArray = dog3.getDogArray();
+		System.out.print("Our third dog is: ");
+		for(int i = 0; i < dogArray.length; i++) {
+			System.out.print(dogArray[i] + ", ");
+		}
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Congratulations to our canine graduates!");
+	}
+}
